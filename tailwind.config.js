5f75a4930a6916e869999b5bc52412229fb25385
Kristea Gamel <krgamel@gmail.com>
2022-03-27 15:47:53 -0500

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+       
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+  ],
+};
